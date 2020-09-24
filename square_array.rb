1b728b39d43c00d4e2358287d3f2c2c9ee516890
June Young Kang <jkang34@gmail.com>
2020-09-24 02:04:29 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,5 @@
 def square_array(array)
   arrays = []
-  array.each do |number|
-    arrays.push(number**2)
-    return arrays
-  end
+  array.each{|number| arrays.push(number**2)}
+  return arrays
 end
